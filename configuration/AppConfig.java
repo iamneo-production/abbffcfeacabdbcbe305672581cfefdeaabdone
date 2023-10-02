a10b3600e0cb7fcc0188bc0184f92f55ea70762d
Iamneo <techadmin@iamneo.ai>
2023-10-02 09:45:24 +0000

    Backup Commit

--- configuration/AppConfig.java
+++ configuration/AppConfig.java
@@ -1,3 +1,14 @@
+package com.examly.springapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.sprin
+@Configuration
+@ComponentScan(base)
 public class AppConfig {
-    
+    @Bean
+    public Student student(){
+        Student student = new Student();
+        return student;
+    }
 }
